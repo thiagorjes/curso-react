858c363f48fffaa7a8309d87a732e6de358ae145
thiagorjes <thiago_rj_es@hotmail.com>
2021-06-25 23:40:38 -0300

    novas aulas

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -3,6 +3,15 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+BillingCycle.route('count',(req,res,next) => {
+    BillingCycle.count((error, value) => {
+        if(error){
+            res.status(500).json({erros: [error]})
+        }
+    })
+})
+
+
 BillingCycle.route('get', (req, res, next) => {
     BillingCycle.find({}, (err, docs) => {
         if (!err) {
